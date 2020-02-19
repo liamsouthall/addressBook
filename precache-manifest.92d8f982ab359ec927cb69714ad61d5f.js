aeb8641d974dabfbfddda502388b2acd0968ae83
CN20 <codenation@CN20s-MacBook-Pro.local>
2020-02-19 14:25:19 +0000

    Updates

---
+++ precache-manifest.92d8f982ab359ec927cb69714ad61d5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8c9c16b6be27a8bced91272d3d94075",
+    "url": "/addressBook/index.html"
+  },
+  {
+    "revision": "fabf94dbee7e2fe06686",
+    "url": "/addressBook/static/css/main.9d79d0f9.chunk.css"
+  },
+  {
+    "revision": "61a11596760d15ce6321",
+    "url": "/addressBook/static/js/2.80916bbe.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/addressBook/static/js/2.80916bbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fabf94dbee7e2fe06686",
+    "url": "/addressBook/static/js/main.d14e7ded.chunk.js"
+  },
+  {
+    "revision": "2c0ba4c24141be2a8c6a",
+    "url": "/addressBook/static/js/runtime-main.9841e49c.js"
+  }
+]);
