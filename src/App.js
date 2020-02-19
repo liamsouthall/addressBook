bd339f1ffbd23fdf8dd62d0e8265e4c9df0a56e0
CN20 <codenation@CN20s-MacBook-Pro.local>
2020-02-19 14:11:47 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    numbers: [],
+    current: "",
+    inputSwitch: true,
+    tempObj: { name: "", number: "" }
+  };
+
+  enterHandler = event => {
+    let storedNums = [...this.state.numbers];
+    let tempObj1 = this.state.tempObj;
+    if (event.key === "Enter") {
+      if (this.state.inputSwitch === true) {
+        tempObj1.name = this.state.current;
+        this.setState({ tempObj: tempObj1, current: "", inputSwitch: false });
+        console.log(this.state.numbers);
+        console.log(this.state.tempObj);
+      } else {
+        tempObj1.number = this.state.current;
+        this.setState({ tempObj: tempObj1 });
+        console.log(this.state.tempObj);
+        storedNums.push(this.state.tempObj);
+        console.log(storedNums);
+        this.setState({ numbers: storedNums, current: "", inputSwitch: true });
+      }
+    }
+  };
+
+  changeHandler = event => {
+    this.setState({ current: event.target.value });
+  };
+
+  delete = index => {
+    this.setState(this.state.numbers.splice(index, 1));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Address Book</h1>
+        <input
+          type="text"
+          className="Input"
+          placeholder={this.state.inputSwitch ? "Write Name" : "Write Number"}
+          value={this.state.current}
+          onChange={event => {
+            this.changeHandler(event);
+          }}
+          onKeyDown={event => this.enterHandler(event)}
+        ></input>
+        {this.state.numbers.map((num, index) => {
+          return (
+            <div key={index}>
+              <p>{num.name}</p>
+              <p>{num.number}</p>
+              <button onClick={() => this.delete(index)}>x</button>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
